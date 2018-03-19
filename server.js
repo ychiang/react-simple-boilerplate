c47d7e36813909088758360045a38e3a18d88497
Grace Chiang <gracechiang@me.com>
2018-03-19 18:56:35 +0000

    added package-lock.json to gitignore

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
+var ReactDOMServer = require('react-dom/server');
 
 new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
